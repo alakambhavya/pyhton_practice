ca70b0fdb7d3bb9baa0c955903f9b2d0b9f8e3f5
alakambhavya <alakambhavya@gmail.com>
2026-05-28 16:40:31 +0530

    update

--- num.py
+++ num.py
@@ -6,3 +6,17 @@ print(complex(num))
 print(type(complex(num)))
 print(float(num))
 print(type(float(num)))
+
+print("________________________")
+
+#build a salary bonus calculator using arithmetic operators.
+emp_1=15000
+emp_2=30000
+bonus=5000
+add=emp_1+bonus
+print(add)
+print(type(add))
+
+
+
+
