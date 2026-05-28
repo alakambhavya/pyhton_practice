47f1c16e8bce5d4ab53c035717e195cdabc08c38
alakambhavya <alakambhavya@gmail.com>
2026-05-28 22:15:25 +0530

    dt,op,index,slicing

--- num.py
+++ num.py
@@ -7,7 +7,7 @@ print(type(complex(num)))
 print(float(num))
 print(type(float(num)))
 
-print("________________________")
+print("_______________________________________________")
 
 #task2:build a salary bonus calculator using arithmetic operators.
 emp_1=15000
@@ -17,7 +17,7 @@ add=emp_1+bonus
 print(add)
 print(type(add))
 
-print("__________________")
+print("___________________________________________________")
 
 #task3:write a comparison program for product prices.
 product_1=30
@@ -42,7 +42,7 @@ print(product_1>=product_2)
 print(product_1>=product_3)
 print(product_2>=product_3)
 
-print("_________________________")
+print("__________________________________________________")
 
 #task4:build a string indexing application.
 name="alakam bhavya sree"
@@ -51,25 +51,16 @@ print(name[7])
 print(name[5])
 print(name[14])
 
-print("_______________________________") 
+print("______________________________________________________-") 
 
-#task5:Write a program to rotate list elements using slicing.
-std=[1,2,3,4,5,6,7,8,9,10,"bhavya",387,'suchi',2390]
-print(std[-1::-1])
+#task5:Write a slicing program to extract usernames from emails.
+mail="alakambhavya@gmail.com,alakamnandhakishore@gmail.com"
+print(mail[0:12:1])
+print(mail[23:42:1]) 
 
-print("_______________________________")
+print("__________________________________________")
 
-
-#task6:create a program to reverse a string using slicing.
-data="thinking provides knowledge, and knowledge makes you great."
-print(data[-33:-42:-1])
-print(type(data))
-print(data[-43:-51:-1])
-print(type(data))
-
-print("_________________________________")
-
-#task7:Build a marks update system using assignment operators.
+#task6:Build a marks update system using assignment operators.
 std="bhavya"
 marks=79
 marks=5
@@ -88,6 +79,48 @@ marks%=7
 print(marks)
 marks**=3
 print(marks)
+
+print("_____________________________________________")
+
+#task7:write a program to manage employee records using dictionaries.
+employee={"emp_records":({"emp_id":"3201","emp_name":"bhavya","emp_salary":20000},
+                         {"emp_id":"3202","emp_name":"suchi","emp_salary":40000},
+                         {"emp_id":"3203","emp_name":"bhanu","emp_salary":50000})}
+print(employee["emp_records"])
+print(employee["emp_records"][0]["emp_name"])
+
+print("_________________________________________")
+
+#task9:Build a program demonstrating set data types  
+fruits={"banana","apple","orange",}
+print(fruits)
+fruits.add("grapes")
+print(fruits)
+fruits.remove("banana")
+print(fruits)
+
+#task10:Write a program to rotate list elements using slicing.
+std=[1,2,3,4,5,6,7,8,9,10,"bhavya",387,'suchi',2390]
+print(std[-1::-1])
+
+print("____________________________________________________")
+
+
+#task11:create a program to reverse a string using slicing.
+data="thinking provides knowledge, and knowledge makes you great."
+print(data[-33:-42:-1])
+print(type(data))
+print(data[-43:-51:-1])
+print(type(data))
+
+print("_______________________________________________")
+
+
+
+ 
+
+
+
 
 
 
