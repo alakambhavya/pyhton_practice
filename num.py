dda137d0fbb724aad22b29e053ba399f23aad653
alakambhavya <alakambhavya@gmail.com>
2026-05-28 16:36:53 +0530

    datatypes,operators,indexing,slicing

---
+++ num.py
@@ -0,0 +1,8 @@
+#Create a program to convert one data type into another  
+num=267
+print(num)
+print(type(num))
+print(complex(num))
+print(type(complex(num)))
+print(float(num))
+print(type(float(num)))
